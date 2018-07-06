8b2e42c84bb47625d61d2eae21952b163fbf55c4
AnaMaria Ioan | eMAG, Software Development <anamaria.ioan@emag.ro>
2018-07-06 19:09:22 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+    <title>CAML</title>
+
+</head>
+<body>
+
+    <div class="row">
+        <div class="col-md-3" style="background-color:#005eb7;">
+            <div class="media">
+                    <div class="media-left media-middle">
+                        <div><a href="index.php"><img class="logo-icon" src="imgs/logo.png"></a></div>
+                    </div>
+                    <div class="media-body">
+                        <h1 class="logo">CAML</h1>
+                    </div>
+            </div>
+        </div>
+
+        <div class="col-md-6" style="background-color:#005eb7;">
+        </div>
+
+        <div class="col-md-3" style="background-color:#005eb7;">
+            <div class="col-md-12 row">
+                <div class="col-md-8">
+                    <form action="" method="GET" class="form-group row">
+                            <div class="col-md-8">
+                                <label class="sr-only" for="search">Search</label>
+                                <div class="input-group">
+                                    <input type="text" class="form-control input-search" name="q" id="search" placeholder="Search...">
+                                </div>
+                            </div>
+                            <div class="col-md-4">
+                                <button type="submit" class="btn">
+                                    <span class="fa fa-search" aria-hidden="true"> Search </span>
+                                </button>
+                            </div>
+                </div>
+                
+                <div class="col-md-4">
+                    <p>My account</p>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
