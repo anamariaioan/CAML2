166fddb2c059132561a85f54dd76cb4bbb7a6c81
AnaMaria Ioan | eMAG, Software Development <anamaria.ioan@emag.ro>
2018-07-08 15:11:26 +0300

    header done

--- index.php
+++ index.php
@@ -6,8 +6,10 @@
     <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet"/>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <title>CAML</title>
 
@@ -15,7 +17,7 @@
 <body>
 
     <div class="row">
-        <div class="col-md-3" style="background-color:#005eb7;">
+        <div class="col-md-3 form-inline" style="background-color:#005eb7;">
             <div class="media">
                     <div class="media-left media-middle">
                         <div><a href="index.php"><img class="logo-icon" src="imgs/logo.png"></a></div>
@@ -29,25 +31,15 @@
         <div class="col-md-6" style="background-color:#005eb7;">
         </div>
 
-        <div class="col-md-3" style="background-color:#005eb7;">
-            <div class="col-md-12 row">
-                <div class="col-md-8">
-                    <form action="" method="GET" class="form-group row">
-                            <div class="col-md-8">
-                                <label class="sr-only" for="search">Search</label>
-                                <div class="input-group">
-                                    <input type="text" class="form-control input-search" name="q" id="search" placeholder="Search...">
-                                </div>
-                            </div>
-                            <div class="col-md-4">
-                                <button type="submit" class="btn">
-                                    <span class="fa fa-search" aria-hidden="true"> Search </span>
-                                </button>
-                            </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <p>My account</p>
+        <div class="col-md-3 row form-inline" style="background-color:#005eb7;">
+            <div class="dropdown" style="margin-left: 60%">
+                <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <span class="fa fa-user"></span>
+                    My account
+                </button>
+                <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <a class="dropdown-item" href="#">Profile</a>
+                    <a class="dropdown-item" href="#">Log out</a>
                 </div>
             </div>
         </div>
