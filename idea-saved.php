24875eb704ef1ee9aa180fd0d90f1e99d92393d2
AnaMaria Ioan | eMAG, Software Development <anamaria.ioan@emag.ro>
2018-07-24 20:01:55 +0300

    idea saved page - done

--- idea-saved.php
+++ idea-saved.php
@@ -25,5 +25,22 @@ require __DIR__ . "/dosar.php";
 
     ?>
 
+    <div class="body jumbotron ">
+        <div class="text-center crimson">
+            <h4>
+                Yey! The Caml is happy and it will try to bring you a lot of votes. <a href="index.php"><img src="imgs/caml-logo.png" class="logo-caml"></a>
+            </h4>
+            <h5>
+                Meanwhile, enjoy a cup of coffee or tea. We will let you know if your idea is a winning one.
+            </h5>
+        </div>
+        <div>
+            </br></br></br>
+        </div>
+        <div class="text-right" style="color: darkgray">
+            <h6>Tip: The Caml knows some tricks. Click it!</h6>
+        </div>
+    </div>
+
 </body>
 </html>
